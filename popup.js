1dfe5580b155d0c16366e785ab40fe715f8c48b3
mmarszal7 <mmarszal7@gmail.com>
2023-05-31 17:10:26 +0200

    add missing semicolons

--- popup.js
+++ popup.js
@@ -3,14 +3,14 @@ chrome.storage.sync.get("urls", function (x) {
     if (x.urls && x.urls.length > 0) {
         urls = x.urls;
         for (let i = 0; i < urls.length; i++) {
-            drawRow(i, urls[i])
+            drawRow(i, urls[i]);
         }
     }
 });
 
 let saveButton = document.getElementById("save");
 saveButton.addEventListener("click", async () => {
-    addNewUrl()
+    addNewUrl();
 });
 
 let newUrlInput = document.getElementById("newUrl");
@@ -23,36 +23,36 @@ newUrlInput.addEventListener("keypress", function (event) {
 function addNewUrl() {
     const newUrl = document.getElementById("newUrl").value;
     document.getElementById("newUrl").value = "";
-    urls.push(newUrl)
-    drawRow(maxId(), newUrl)
-    updateBlocker(urls)
+    urls.push(newUrl);
+    drawRow(maxId(), newUrl);
+    updateBlocker(urls);
 }
 
 function maxId() {
-    let max = 0
+    let max = 0;
     for (let i = 0; i < document.getElementById('container').children.length - 1; i++) {
         const id = document.getElementById('container').children[i].id;
         if (id > max) {
-            max = id
+            max = id;
         }
     }
     return max + 1;
 }
 
 function drawRow(id, value) {
-    document.getElementById('container').insertAdjacentHTML('afterbegin', `<div id="${id}" class="row"> <input type="text" placeholder="URL" value="${value}"/> <button class="delete">Delete</button> </div>`)
-    const deleteButton = document.getElementById(id).getElementsByTagName('button')[0]
+    document.getElementById('container').insertAdjacentHTML('afterbegin', `<div id="${id}" class="row"> <input type="text" placeholder="URL" value="${value}"/> <button class="delete">Delete</button> </div>`);
+    const deleteButton = document.getElementById(id).getElementsByTagName('button')[0];
     deleteButton.addEventListener("click", async (e) => {
-        const rowToRemove = document.getElementById(e.target.parentElement.id)
-        const urlToRemove = rowToRemove.getElementsByTagName('input')[0].value
+        const rowToRemove = document.getElementById(e.target.parentElement.id);
+        const urlToRemove = rowToRemove.getElementsByTagName('input')[0].value;
         urls.splice(urls.indexOf(urlToRemove), 1);
-        rowToRemove.remove()
-        updateBlocker(urls)
+        rowToRemove.remove();
+        updateBlocker(urls);
     });
 }
 
 function updateBlocker(newUrls) {
-    chrome.storage.sync.set({ urls: newUrls })
+    chrome.storage.sync.set({ urls: newUrls });
 
     chrome.declarativeNetRequest.getDynamicRules((rules) => {
         rules.forEach(rule => {
